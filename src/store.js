c4917f90083c3baa2ce0aa71aea487db18b69326
konstantin_zozulya <konstantin.z@orot-technologies.co.il>
2019-05-23 14:31:26 +0300

    remove compose enhancers

--- src/store.js
+++ src/store.js
@@ -4,11 +4,12 @@ import rootReducer from './application/reducers/IndexReducer';
 import { userInfoUpdater } from './application/middleware';
 
 export default ( initialState, isClient = true) => {
-    
+    /*
     var composeEnhancers = compose;
     if (isClient){
         composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
     }
-    
     return createStore(rootReducer, initialState , composeEnhancers(applyMiddleware(thunk, userInfoUpdater)))
+    */
+    return createStore(rootReducer, initialState , compose(applyMiddleware(thunk, userInfoUpdater)))
 }
