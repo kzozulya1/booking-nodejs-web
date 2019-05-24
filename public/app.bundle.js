62addf92b0a14c85a2332ece027d36d6e9f956f1
konstantin_zozulya <konstantin.z@orot-technologies.co.il>
2019-05-24 18:09:50 +0300

    added real ip adress for api

--- public/app.bundle.js
+++ public/app.bundle.js
@@ -889,7 +889,7 @@ eval("// style-loader: Adds some css to the DOM by adding a <style> tag\n\n// lo
 /*! exports provided: apiPrefix, imageServiceUpload, imageServiceDownload, port, lang, default */
 /***/ (function(module) {
 
-eval("module.exports = {\"apiPrefix\":\"http://docker.local:830\",\"imageServiceUpload\":\"http://docker.local:815/upload\",\"imageServiceDownload\":\"http://docker.local:815/download\",\"port\":88,\"lang\":\"ru\"};\n\n//# sourceURL=webpack:///./application/etc/config.json?");
+eval("module.exports = {\"apiPrefix\":\"http://95.181.178.240:830\",\"imageServiceUpload\":\"http://95.181.178.240:815/upload\",\"imageServiceDownload\":\"http://95.181.178.240:815/download\",\"port\":88,\"lang\":\"ru\"};\n\n//# sourceURL=webpack:///./application/etc/config.json?");
 
 /***/ }),
 
